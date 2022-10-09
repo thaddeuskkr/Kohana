601fda560f8aff27ccc95b32b73c3ea0f30bedb0
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-10-09 14:48:55 +0000

    feat(pause): check if already paused
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/commands/music/pause.js
+++ src/commands/music/pause.js
@@ -21,6 +21,7 @@ export class PauseCommand extends Command {
     
     async chatInputRun(interaction) {
         const dispatcher = this.container.client.queue.get(interaction.guildId);
+        if (dispatcher.player.paused === true) return interaction.reply({ embeds: [this.container.client.util.errorEmbed(`**${dispatcher.current.info.title}** - **${dispatcher.current.info.author}** is already paused.`)] });
         await dispatcher.player.setPaused(true);
         await interaction.reply({ embeds: [this.container.client.util.successEmbed(`Paused **${dispatcher.current.info.title}** - **${dispatcher.current.info.author}**.`)] });
     }
