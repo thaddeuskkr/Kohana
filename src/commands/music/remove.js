36b0578aa48d1fdc32c67cabb880805530e244cf
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-10-10 12:58:22 +0800

    fix(remove): reject properly
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/commands/music/remove.js
+++ src/commands/music/remove.js
@@ -29,7 +29,7 @@ export class RemoveCommand extends Command {
         const index = interaction.options.getInteger('index');
         const dispatcher = this.container.client.queue.get(interaction.guildId);
         if (!dispatcher.queue.length) return interaction.reply({ embeds: [this.container.client.util.errorEmbed('There are no tracks in the queue.')] });
-        if (index < 1 || index > dispatcher.queue.length - 1) await interaction.reply({ embeds: [this.container.client.util.errorEmbed(`Invalid queue index (accepts **1** to **${dispatcher.queue.length - 1}**).`)] });
+        if (index < 1 || index > dispatcher.queue.length) return interaction.reply({ embeds: [this.container.client.util.errorEmbed(`Invalid queue index (accepts **1** to **${dispatcher.queue.length}**).`)] });
         const track = dispatcher.queue.splice(index - 1, 1)[0];
         await interaction.reply({ embeds: [this.container.client.util.successEmbed(`Removed **${track.info.title}** - **${track.info.author}** from the queue.`)] });
     }
