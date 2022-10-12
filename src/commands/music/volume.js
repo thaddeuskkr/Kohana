a0087fc2d15d0e6ad93bad321bd9fa7a328cc62b
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-10-12 19:01:12 +0800

    feat(volume): volume command
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

---
+++ src/commands/music/volume.js
@@ -0,0 +1,42 @@
+import { Command } from '@sapphire/framework';
+
+export class VolumeCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'volume',
+            description: 'Changes the volume of the player.',
+            preconditions: ['voice', 'sameVoice', 'dispatcher']
+        });
+    }
+
+    registerApplicationCommands(registry) {
+        registry.registerChatInputCommand((builder) => 
+            builder
+                .setName(this.name)
+                .setDescription(this.description)
+                .setDMPermission(false)
+                .addIntegerOption((option) => 
+                    option
+                        .setName('newVolume')
+                        .setDescription('Sets the new volume of the player (%), accepts an integer between 0 and 200.')
+                        .setRequired(false)
+                )
+        );
+    }
+    
+    async chatInputRun(interaction) {
+        const dispatcher = this.container.client.queue.get(interaction.guildId);
+        const nv = interaction.options.getInteger('newVolume');
+        if (!nv) return interaction.reply({ embeds: [this.container.client.util.embed(`The current volume is ${dispatcher.player.volume * 100}%.`)] });
+        if (!VolumeCommand.inRange(nv, this.container.client.config.minVol || 0, this.container.client.config.maxVol || 200)) {
+            return interaction.reply({ embeds: [this.container.client.util.errorEmbed(`Out of volume range (**${this.container.client.config.minVol}%** to **${this.container.client.config.maxVol}%**)`)] });
+        }
+        dispatcher.player.setVolume(nv / 100);
+        await interaction.reply({ embeds: [this.container.client.util.successEmbed(`Volume set to ${nv}%.`)] });
+    }
+
+    static inRange(x, min, max) {
+        return (x - min) * ( x - max) <= 0;
+    }
+}
