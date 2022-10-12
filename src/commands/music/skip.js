13c610bb2c59b599ff4a3fd124a1161103a047f6
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-10-12 17:06:07 +0800

    fix(skip): workaround for handling loop track
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -24,9 +24,8 @@ export class SkipCommand extends Command {
         await interaction.reply({ embeds: [this.container.client.util.successEmbed(`Skipped ${dispatcher.repeat === 'all' ? 'and removed ' : ''}**${dispatcher.current.info.title}** - **${dispatcher.current.info.author}**${dispatcher.repeat === 'one' ? ' and turned off track repeat' : ''}${dispatcher.repeat === 'all' ? ' from the queue' : ''}.`)] });
         if (dispatcher.repeat === 'one') dispatcher.repeat = 'off';
         if (dispatcher.repeat === 'all') {
-            dispatcher.repeat = 'off';
+            dispatcher.current.skipped = true;
             await dispatcher.player.stopTrack();
-            dispatcher.repeat = 'all';
             return;
         } else {
             dispatcher.player.stopTrack();
